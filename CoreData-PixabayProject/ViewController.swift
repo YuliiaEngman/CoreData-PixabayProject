db68553094211af1d643a75c5911329e1f34116a
Yuliia Engman <yuliia.engman@gmail.com>
2020-04-13 16:07:12 -0400

    code data project to practice

---
+++ CoreData-PixabayProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreData-PixabayProject
+//
+//  Created by Yuliia Engman on 4/13/20.
+//  Copyright © 2020 Yuliia Engman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
